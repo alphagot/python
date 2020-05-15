a5e0e1bf47a6d558962e3e4a27718b1698c10122
alphagogo <alphagogo@outlook.com>
2020-05-15 20:23:12 +0800

    提交

--- 函数/filter.py
+++ 函数/filter.py
@@ -1,1 +1,7 @@
-print(" ---------- ")
+a = list(filter(lambda x: x % 2 == 1, [1, 2, 3, 4, 5, 6]))
+b = tuple(filter(lambda x: x % 2 == 1, (1, 2, 3, 4, 5, 6)))
+c = ["a", "b", "c"]
+d = [1, 2, 3]
+dic = dict(zip(c, d))
+
+print(a, b, dic)

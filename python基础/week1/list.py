d649b452f245a4e272c17a796b21dc1384b1cf9b
alphagogo <26833525+alphagot@users.noreply.github.com>
2020-07-29 22:27:18 +0800

    提交

---
+++ python基础/week1/list.py
@@ -0,0 +1,10 @@
+#__author:  xiaoxinpro13
+#date:  2020/7/28
+
+list_name = ['xiaoming', 'xiaohua', 'xiaozhang']
+# 增删改查
+print(list_name[0])
+
+print(list_name[1:3])
+
+print(list_name[-2::1])
--- python基础/week1/循环.py
+++ python基础/week1/循环.py
@@ -3,9 +3,18 @@
 sum = 0
 for x in range(101):
     sum = sum + x
+
 print(sum)
+
+for i in range(1, 101, 2):
+    print("loop:", i)
+
+for i in range(1, 101):
+    if i < 50 or i > 70:
+        print("loop:", i)
 
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 sum = [x + x for x in a]
 print(sum)
+

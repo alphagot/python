44502781ee5d6c38694b541ed61e5a1daf5f23ba
alphagogo <26833525+alphagot@users.noreply.github.com>
2020-08-11 22:46:31 +0800

    summit

--- python基础/week3/深浅拷贝.py
+++ python基础/week3/深浅拷贝.py
@@ -1,11 +1,12 @@
 # __author:  xiaoxinpro13
 # date:  2020/8/11
+import copy
 
 s = [[1, 2], "Tom", "Alic"]
 
 s2 = s
 
-s3 = s.copy()
+s3 = s.copy()#浅copy
 
 print(s)
 print(s2)
@@ -25,3 +26,7 @@ s[2] = "abc"
 print(s)
 print(s2)
 print(s3)
+
+s4 = copy.deepcopy(s)#深copy
+s[0][1] = 22
+print('------------', s4, s)

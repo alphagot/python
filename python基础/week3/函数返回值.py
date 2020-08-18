3cb6f5f80a2c04c8759bf52f50fbc39bf6562e2a
alphagogo <26833525+alphagot@users.noreply.github.com>
2020-08-18 23:30:26 +0800

    submit

--- python基础/week3/函数返回值.py
+++ python基础/week3/函数返回值.py
@@ -1,3 +1,10 @@
 # __author:  xiaoxinpro13
 # date:  2020/8/18
+# 1.没有返回值的，函数会返回None 执行return就函数结束了
+# 2.如果return多个对象，那么python会把多个对象的返回值封装成一个元组。
+def f():
+    print('--------------')
+    return 1, 1, "aa", (2, 3), [1, 2, 3]
 
+
+print(f())
